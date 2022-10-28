7d722992d1814baaeeeb66c5c74cf66ce0ecb16d
berkayyildirim <huseyinberkayyildirim@gmail.com>
2022-10-29 02:04:15 +0300

    add new rest api to filter houses based on current user

--- routes/house.routes.js
+++ routes/house.routes.js
@@ -47,6 +47,25 @@ router.get("/houses", (req, res, next) => {
     });
 });
 
+// GET /api/my-houses  -  Get list of my houses
+router.get(
+  "/my-houses",
+  isAuthenticated,
+  attachCurrentUser,
+  (req, res, next) => {
+    House.find({ ownerId: req.currentUser._id })
+      .populate("reservations")
+      .then((allHouses) => res.json(allHouses))
+      .catch((err) => {
+        console.log("error getting list of houses...", err);
+        res.status(500).json({
+          message: "error getting list of houses",
+          error: err,
+        });
+      });
+  }
+);
+
 //  GET /api/houses/:houseId -  Retrieves a specific house by id
 router.get("/houses/:houseId", (req, res, next) => {
   const { houseId } = req.params;
@@ -115,45 +134,50 @@ router.put(
 );
 
 // DELETE  /api/houses/:houseId  -  Deletes a specific house by id
-router.delete("/houses/:houseId", isAuthenticated,attachCurrentUser, (req, res, next) => {
-  const { houseId } = req.params;
+router.delete(
+  "/houses/:houseId",
+  isAuthenticated,
+  attachCurrentUser,
+  (req, res, next) => {
+    const { houseId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(houseId)) {
-    res.status(400).json({ message: "Specified id is not valid" });
-    return;
-  }
+    if (!mongoose.Types.ObjectId.isValid(houseId)) {
+      res.status(400).json({ message: "Specified id is not valid" });
+      return;
+    }
 
-  House.findById(req.params.houseId)
-    .then((houseDetails) => {
-      if (
-        req.currentUser._id &&
-        req.currentUser._id.equals(houseDetails.ownerId)
-      ) {
-        House.findByIdAndRemove(houseId)
-          .then(() =>
-            res.json({
-              message: `House with ${houseId} is removed successfully.`,
-            })
-          )
-          .catch((err) => {
-            console.log("error deleting a house...", err);
-            res.status(500).json({
-              message: "error deleting a house",
-              error: err,
+    House.findById(req.params.houseId)
+      .then((houseDetails) => {
+        if (
+          req.currentUser._id &&
+          req.currentUser._id.equals(houseDetails.ownerId)
+        ) {
+          House.findByIdAndRemove(houseId)
+            .then(() =>
+              res.json({
+                message: `House with ${houseId} is removed successfully.`,
+              })
+            )
+            .catch((err) => {
+              console.log("error deleting a house...", err);
+              res.status(500).json({
+                message: "error deleting a house",
+                error: err,
+              });
             });
+        } else {
+          res.status(400).json({
+            message:
+              "Users can delete only thier own data.Please Check the data you are deleting!",
           });
-      } else {
-        res.status(400).json({
-          message:
-            "Users can delete only thier own data.Please Check the data you are deleting!",
-        });
-        return;
-      }
-    })
-    .catch((err) => {
-      console.log("error deleting house details from DB", err);
-      next();
-    });
-});
+          return;
+        }
+      })
+      .catch((err) => {
+        console.log("error deleting house details from DB", err);
+        next();
+      });
+  }
+);
 
 module.exports = router;
